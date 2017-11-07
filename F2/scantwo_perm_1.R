e5c5bfca1eda7de9fc0eadb8c05d6e0492974b0c
Ruijuan Li <ruijuanli@cabernet.genomecenter.ucdavis.edu>
2017-11-06 16:44:21 -0800

    added script to run scantwo permuation for 100 times for Erucic acid and Oleic acid

--- F2/scantwo_perm_1.R
+++ F2/scantwo_perm_1.R
@@ -1,7 +1,7 @@
 library(qtl)
 library(snowfall)
 
-load("/share/malooflab/Ruijuan/F2/QTL_analysis/data/LG.f2.after.crossover.final.Rdata")
+load("/share/malooflab/Ruijuan/F2/QTL_analysis/data/LG.f2.after.crossover_Erucic_Oleic.Rdata")
 
 LG.f2.after.crossover <- sim.geno(LG.f2.after.crossover,step=1,n.draws=32)
 LG.f2.after.crossover <- calc.genoprob(LG.f2.after.crossover,step=1)
@@ -10,16 +10,17 @@ sfInit(parallel = TRUE, cpus = 16)
 sfExport("LG.f2.after.crossover")
 sfLibrary(qtl)
 
-# run scantwo permutation for Erucic acid and Oleic acid
+# run scantwo for all traits at once 
 set.seed(1)
-  
+
 system.time(
 scantwo.perm.imp.1 <- 
 sfLapply(seq_along(LG.f2.after.crossover$pheno), function(trait) {
   print(trait)
-  scantwo(LG.f2.after.crossover,pheno.col=trait,method="hk", n.perm=10, n.cluster = 16)
+  scantwo(LG.f2.after.crossover,pheno.col=trait,method="hk",n.perm=10,n.cluster = 10)
 }) 
 )
+
 names(scantwo.perm.imp.1) <- colnames(LG.f2.after.crossover$pheno)
 
 sfStop()
---
+++ F2/scantwo_perm_8.R
@@ -0,0 +1,29 @@
+library(qtl)
+library(snowfall)
+
+load("/share/malooflab/Ruijuan/F2/QTL_analysis/data/LG.f2.after.crossover_Erucic_Oleic.Rdata")
+
+LG.f2.after.crossover <- sim.geno(LG.f2.after.crossover,step=1,n.draws=32)
+LG.f2.after.crossover <- calc.genoprob(LG.f2.after.crossover,step=1)
+
+sfInit(parallel = TRUE, cpus = 16)
+sfExport("LG.f2.after.crossover")
+sfLibrary(qtl)
+
+# run scantwo for all traits at once 
+set.seed(8)
+
+system.time(
+scantwo.perm.imp.8 <- 
+sfLapply(seq_along(LG.f2.after.crossover$pheno), function(trait) {
+  print(trait)
+  scantwo(LG.f2.after.crossover,pheno.col=trait,method="hk",n.perm=10,n.cluster = 10)
+}) 
+)
+
+names(scantwo.perm.imp.8) <- colnames(LG.f2.after.crossover$pheno)
+
+sfStop()
+
+# save output 
+save(scantwo.perm.imp.8, file = "/share/malooflab/Ruijuan/F2/QTL_analysis/output/scantwo.perm.imp.8.Rdata")
